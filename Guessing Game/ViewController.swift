90bfba532ddcb0f0d7b606bff56f419de055e8d7
Will Epperson <wepperson@wills-macbook-pro.local>
2015-10-10 00:52:49 -0400

    updated code

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var userGuess: UITextField!
+    @IBOutlet var response: UILabel!
+    @IBOutlet var triesLabel: UILabel!
+    
+    var randomNumber = Int(arc4random_uniform(5) + 1)
+    var numTries = 0
+    
+    @IBAction func submit(sender: AnyObject) {
+        
+        if Int(userGuess.text!) == randomNumber {
+            response.text = "Correct! The number was \(randomNumber)"
+        }
+        else {
+            response.text = "Try again!"
+            numTries++
+        }
+        
+        triesLabel.text = "Tries: \(numTries)"
+        
+    }
 
+    @IBAction func reset(sender: AnyObject) {
+        randomNumber = Int(arc4random_uniform(5) + 1)
+        numTries = 0
+        response.text = ""
+        triesLabel.text = "Tries: \(numTries)"
+    }
+    
+    // X-code generated
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        triesLabel.text = "Tries: \(numTries)"
     }
 
     override func didReceiveMemoryWarning() {
